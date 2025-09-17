a30dc9e701578e2e77a95d4dbf0e58259ce99459
Thomas von Dein <tom@vondein.org>
2025-09-17 21:04:24 +0200

    fix version

--- config.go
+++ config.go
@@ -125,7 +125,7 @@ var Templates = map[string]string{
 }
 
 const (
-	VERSION        string = "0.0.9"
+	VERSION        string = "0.0.8"
 	DefaultCount   int    = 160 // number of words to generate if -c is omitted
 	DefaultColumns int    = 10  // number of columns to print
 	MaxWidth       int    = 72  // max width of output, adjusts columns
